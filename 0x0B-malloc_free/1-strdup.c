c401716599020ef631ef9ebc135e0fa4e438c897
Fatima20027 <fatima.hailou@icloud.com>
2023-07-11 16:57:21 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,51 +2,35 @@
 #include <stdlib.h>
 
 /**
-  * string_nconcat - ...
-  * @s1: ...
-  * @s2: ...
-  * @n: ...
+  * _strdup - Duplicate a string
+  * @str: the string to duplicate
   *
-  * Return: ...
+  * Return: the string duplicated
   */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *_strdup(char *str)
 {
-	unsigned int i = 0, j = 0, k = 0, l = 0;
-	char *str;
+	int a = 0, i = 1;
+	char *s;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	while (s1[i])
-		i++;
-
-	while (s2[k])
-		k++;
-
-	if (n >= k)
-		l = i + k;
-	else
-		l = i + n;
-
-	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
 
-	k = 0;
-	while (j < l)
+	while (str[i])
 	{
-		if (j <= i)
-			str[j] = s1[j];
-
-		if (j >= i)
-		{
-			str[j] = s2[k];
-			k++;
-		}
-		j++;
+		i++;
 	}
-	str[j] = '\0';
-	return (str);
+
+	s = malloc((sizeof(char) * i) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	while (a < i)
+	{
+		s[a] = str[a];
+		a++;
+	}
+
+	s[a] = '\0';
+	return (s);
 }
