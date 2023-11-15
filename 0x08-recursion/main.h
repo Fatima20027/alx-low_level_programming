398c82a91507bf7fe2cdd139f4cfc0403cd217bf
Fatima20027 <fatima.hailou@icloud.com>
2023-11-15 20:04:34 +0100

    0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+#include <stdio.h>
+#include <unistd.h>
+
+
+void _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
