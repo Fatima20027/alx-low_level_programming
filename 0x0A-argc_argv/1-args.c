26b58ecb07c6d7fd57e4fac5acf75d02758fcb93
Fatima20027 <fatima.hailou@icloud.com>
2023-11-17 20:48:42 +0100

    number

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,16 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
-  * main - prints the number of arguments passed into it
-  * @argc: argument count
-  * @argv: argument vector
-  *
-  * Return: Always zero
-  */
+  
+main - prints the number of arguments passed into it
+@argc: argument count
+@argv: argument vector
+*
+Return: Always zero
+*/
 
-int main(int argc , chat *argv[])
-{ 
-	(void) argc;
-	printf("%s\n", argc - 1);
-
+int main(int argc , __atribute__((unused)) char *argv[])
+{
+        printf("%d\n", argc - 1);
+        return (0);
 }
