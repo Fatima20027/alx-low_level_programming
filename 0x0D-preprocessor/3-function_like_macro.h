017e68cb1ebc07eb922eca07c586989498f40c7c
Fatima20027 <fatima.hailou@icloud.com>
2023-11-27 20:50:44 +0100

    3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,15 @@
+#include <stdio.h>  
+#include <stdlib.h>
+
+
+/**
+ *  function-like macro that computes the absolute value of a number
+ *
+ *  Returns: always zero
+ */
+
+int main()  
+{   
+    printf (" x %d ", abs (x));   
+    return 0;  
+}  
