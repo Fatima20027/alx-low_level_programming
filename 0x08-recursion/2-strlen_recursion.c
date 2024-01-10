15ccc2463190a9c93f986d68ea2b90e6fcf51c03
Fatima20027 <fatima.hailou@icloud.com>
2024-01-10 18:43:10 +0100

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,16 +1,20 @@
 #include "main"
 
 /**
- * _strlen_recursion - return the length of a string
- * @strlen : ...
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
  */
+int _strlen_recursion(char *s)
+{
+        int longit = 0;
 
+        if (*s)
+        {
+                longit++;
+                longit += _strlen_recursion(s + 1);
+        }
 
-int _strlen_recursion(char *s) {
-	if (*s == '\0') {
-        return 0;
-    } else {
-  
-        return 1 + _strlen_recursion(s + 1);
-    }
+        return (longit);
 }
