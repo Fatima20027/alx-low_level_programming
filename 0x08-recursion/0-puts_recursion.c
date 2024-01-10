2afb322441c1d1212d16b8924eee0a5f4032f8dc
Fatima20027 <fatima.hailou@icloud.com>
2024-01-10 17:04:01 +0100

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,13 @@
   */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_put_recursion(s + 1)
+		
 	}
+	else
+		_putchar("\n");
 
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
 }
