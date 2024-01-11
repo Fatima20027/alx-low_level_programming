e3c3cbe66f7a9b8611a0c31ec407090aab2fb0e1
Fatima20027 <fatima.hailou@icloud.com>
2024-01-11 13:48:53 +0100

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
-  * _puts_recursion - Print a string followed by a new line
+  * _puts_recursion - Print a string 
   * @s: the string to print
   *
   * Return: Nothing.
   */
+
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 		_putchar(*s);
-		_put_recursion(s + 1)
-		
+		_put_recursion(s + 1);
 	}
 	else
 		_putchar("\n");
