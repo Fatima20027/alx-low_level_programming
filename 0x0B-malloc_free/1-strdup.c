81f8d3095b115d5c362a1c82d77cb40720000206
fatima20027 <fatima.hailou@icloud.com>
2024-01-16 13:23:55 +0100

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,3 @@
-#include <string.h>
 #include <stdlib.h>
 #include "main.h"
 
@@ -10,17 +9,23 @@
   */
 
 char *_strdup(char *str) 
-{	int i = 0, size = 0;
+{	
+	int i = 0, size = 0;
 	char *s;
+
     	if (str == NULL) {
         	return (NULL);
     }
-   	size_t length = strlen(str);
-    	s = malloc((length + 1) * sizeof(char)); 
+   	for (str[size] != '\0'; size++)
+    	s = malloc(size * sizeof(*str)+1); 
     	if (s == NULL) 
 	{
         	return (NULL);
     }
-    strcpy(s, str);
-    	return (str);
+	else
+	{
+		for (i = 0; i < size; i++)
+			s[i] = str[i];
+	}
+    	return (s);
 }
