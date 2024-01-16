d6d459b9a06dacc2f2aec9a68acb8dbfcd00430f
fatima20027 <fatima.hailou@icloud.com>
2024-01-16 13:51:53 +0100

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,7 +10,7 @@
 
 char *_strdup(char *str) 
 {	
-	int i 
+	int i; 
 	char *s;
 
     	if (str == NULL) 
@@ -18,7 +18,7 @@ char *_strdup(char *str)
         	return (NULL);
 	}
    	for (size = 0; str[size] != '\0'; size++)
-    	s = malloc(size * sizeof(char)+1); 
+    	s = malloc(size * sizeof(char) + 1); 
     	if (s == NULL) 
 	{
         	return (NULL);
