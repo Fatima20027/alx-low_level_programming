6eb9553572fe63f6987d158925e38220e551d0c5
fatima20027 <fatima.hailou@icloud.com>
2024-01-22 21:45:20 +0100

    2.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "0-object_like_macro.h"
+#include "1-pi.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+	int f;
+	f = __FILE__;
+	printf("%d\n", f);
+
+	return (0);
+}
