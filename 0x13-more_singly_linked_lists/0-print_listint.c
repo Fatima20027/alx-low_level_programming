24fa2d644b77d4b6fdb045fcdcb24592b54cc95b
Fatima20027 <fatima.hailou@icloud.com>
2024-02-05 21:20:39 +0100

    0-print was updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,6 +6,7 @@
  *
  * Return: the number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
 const listint_t *cursor = h;
@@ -13,9 +14,9 @@ size_t count = 0;
 
 while (cursor != NULL)
 {
-printf("%d\n", cursor->n);
-count += 1;
-cursor = cursor->next;
+	printf("%d\n", cursor->n);
+	count += 1;
+	cursor = cursor->next;
 }
 return (count);
 }
