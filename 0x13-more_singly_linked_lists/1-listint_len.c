5be4ae1a16ae42a159b06ff0d05b313f25218e34
Fatima20027 <fatima.hailou@icloud.com>
2024-02-05 20:50:43 +0100

    1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,20 @@
 #include "lists.h"
 
+/**
+ * listint_len - Counts the number of elements in a linked list
+ * @h: A pointer to the head of the list
+ *
+ * Return: The number of elements in the list
+ */
+
 size_t listint_len(const listint_t *h)
 {
-	int len =0;
 
-    while(head != NULL){
+	size_t len =0;
 
-        head = head->next;
+    while(h != NULL){
+
+        h = h->next;
 
         len++;
 
