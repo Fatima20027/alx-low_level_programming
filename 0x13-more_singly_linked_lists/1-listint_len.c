6dea19f6593e0219c50411a5b10e93b9b811bf28
Fatima20027 <fatima.hailou@icloud.com>
2024-02-05 21:16:42 +0100

    fixing 1-listint_len betty

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,16 +9,13 @@
 
 size_t listint_len(const listint_t *h)
 {
+	size_t len = 0;
 
-	size_t len =0;
+	while (h != NULL)
+	{
+	h = h->next;
+	len++;
+	}
 
-    while(h != NULL){
-
-        h = h->next;
-
-        len++;
-
-    }
-
-    return len;
+	return (len);
 }
