2945ee4c9d49e1f1edbaf0d95feeeede291ee025
Fatima20027 <fatima.hailou@icloud.com>
2024-02-12 13:35:51 +0100

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,27 +7,26 @@
 *
 * Return: 1 on success, -1 on failure
 */
-
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i;
+    int i = 0, c;
 
-	if (filename == NULL)
-		return (-1);
+    if (filename == NULL)
+        return (-1);
+    if (text_content == NULL)
+        text_content = "";
 
-	if (text_content == NULL)
-		return ("");
-
-	for (i = 0; text_content[i]; i++);
+    while (text_content[i] != '\0')
+        i++;
 
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+    c = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (c == -1)
-		return (-1);
+    if (c == -1)
+        return (-1);
 
-	write(fd, text_content, i);
 
-	return (1);
+    write(c, text_content, i);
+
+    return (1);
 }
-
